12e8bc7e71f86533457b2f598a280211720f6c98
frontendfunn <frontendfunn@gmail.com>
2020-01-24 23:11:23 +0530

    bootstrap 4 sample admin template - frontendfunn

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# 🙈 🙉 🙊 Bootstrap 4 Sample Admin Template
+
+## 😃 Click here for the 👉 [💥LIVE DEMO]()
+
+---
+
+![Preview](./images/preview.png)
+
+# 👉 Subscribe to My Channel [💙❤️Youtube❤️💙](https://www.youtube.com/channel/UCpOHt5d6GG-mvo-_pU06rhQ?sub_confirmation=1)
+
+Made with ❤️ - by [FrontEndFunn](https://www.youtube.com/channel/UCpOHt5d6GG-mvo-_pU06rhQ?sub_confirmation=1)
---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('.sideMenuToggler').on('click', function() {
+    $('.wrapper').toggleClass('active');
+  });
+
+  var adjustSidebar = function() {
+    $('.sidebar').slimScroll({
+      height: document.documentElement.clientHeight - $('.navbar').outerHeight()
+    });
+  };
+
+  adjustSidebar();
+  $(window).resize(function() {
+    adjustSidebar();
+  });
+});
